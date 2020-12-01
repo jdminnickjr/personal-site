acc04c43280611dd689e5260526db946cb8ff69b
JD Minnick <JD@Sidneys-MacBook-Pro-2.local>
2020-11-30 17:24:53 -0700

    Clean up js on mobile

--- animations.js
+++ animations.js
@@ -1,28 +1,13 @@
-// const outerRing = document.querySelector(".outer-ring");
-// document.addEventListener("scroll", function () {
-//   console.log(outerRing);
-//   outerRing.setAttribute("transform", "rotate(180 290 290)");
-// });
-
-// var outerRing = document.getElementById("outer-ring");
-// var middleRing = document.getElementById("middle-ring");
-// var innerRing = document.getElementById("inner-ring");
-// var inner = document.getElementById("inner");
-
-// window.addEventListener("scroll", function () {
-//   outerRing.style.transform = "rotate(" + window.pageYOffset + "deg)";
-//   middleRing.style.transform = "rotate(-" + window.pageYOffset /  + "deg)";
-//   innerRing.style.transform = "rotate(" + window.pageYOffset + "deg)";
-//   inner.style.transform = "rotate(" + window.pageYOffset + "deg)";
-// });
-
 window.onscroll = function () {
   scrollRotate();
 };
 
 function scrollRotate() {
   const outerRing = document.getElementById("outer-ring");
-  outerRing.style.transform = "rotate(" + window.pageYOffset / 2 + "deg)";
+  outerRing.setAttribute(
+    "transform",
+    "rotate(" + window.pageYOffset / 2 + " 290 290)"
+  );
   const middleRing = document.getElementById("middle-ring");
   middleRing.setAttribute(
     "transform",
