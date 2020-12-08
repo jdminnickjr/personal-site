c543f76d3d720695ec0341f8a942c85c49081c0f
JD Minnick <jdminnickjr@gmail.com>
2020-12-07 17:33:38 -0700

    Set Ring variables outside of calls re: #18

--- animations.js
+++ animations.js
@@ -1,19 +1,22 @@
+const outerRing = document.getElementById("outer-ring");
+const middleRing = document.getElementById("middle-ring");
+const innerRing = document.getElementById("inner-ring");
+
 window.onscroll = function () {
   scrollRotate();
 };
 
 function scrollRotate() {
-  const outerRing = document.getElementById("outer-ring");
   outerRing.setAttribute(
     "transform",
     "rotate(" + window.pageYOffset / 2 + " 290 290)"
   );
-  const middleRing = document.getElementById("middle-ring");
+
   middleRing.setAttribute(
     "transform",
     "rotate(" + -window.pageYOffset / 2 + " 290 290) translate(45 45)"
   );
-  const innerRing = document.getElementById("inner-ring");
+
   innerRing.setAttribute(
     "transform",
     "translate(34 34) rotate(" + window.pageYOffset / 2 + " 117 117)"
